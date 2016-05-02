aaaecc7a8ae0ef34605f2fd483938da4b2b17959
Yalcin Ozhabes <yalcinozhabes@gmail.com>
2016-05-01 23:45:11 -0400

    trying to fix __init__.py

--- jdftx/__init__.py
+++ jdftx/__init__.py
@@ -1,8 +1,8 @@
-from .jdftx_unitcell import nuc as jdnuc
-from .jdftx_lattice import lattice as jdlat
-from .jdftx_output import output as jdout
+from jdftx_unitcell import nuc as jdnuc
+from jdftx_lattice import lattice as jdlat
+from jdftx_output import output as jdout
 
-from .jdftx_constants import bohr_as_angstrom, hartree_as_eV, \
+from jdftx_constants import bohr_as_angstrom, hartree_as_eV, \
                              angstrom_as_bohr, eV_as_hartree
 
 __all__ = ['jdnuc', 'jdlat', 'jdout',
