47a301abb53401e4140b0f3cf4e297b22013371e
Yalcin Ozhabes <yalcinozhabes@gmail.com>
2016-05-02 02:20:35 -0400

    pass profile settings to jdftx.py

--- jdftx/jdftx.py
+++ jdftx/jdftx.py
@@ -4,7 +4,19 @@ from jdftx_output import output as jdout
 
 from jdftx_constants import bohr_as_angstrom, hartree_as_eV, \
                              angstrom_as_bohr, eV_as_hartree
+                             
+from IPython import get_ipython
+ipython = get_ipython()
 
-__all__ = ['jdnuc', 'jdlat', 'jdout',
-           'bohr_as_angstrom', 'hartree_as_eV',
-           'angstrom_as_bohr', 'eV_as_hartree']
+import numpy as np
+import io
+import matplotlib as mpl
+#~ mpl.use('nbagg')
+ipython.magic("matplotlib inline")
+#~ ipython.magic("matplotlib nbagg")
+del ipython
+
+import matplotlib.pyplot as plt 
+import prettyplotlib as ppl
+mpl.rcParams['figure.figsize']=[10,5]
+mpl.rcParams['font.size']=13
