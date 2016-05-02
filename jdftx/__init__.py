50afcd123b5ec0191e271a5e994aa7d7402b66c6
Yalcin Ozhabes <ao294@cornell.edu>
2016-05-01 21:02:23 -0400

    working on jdftx scripts

--- jdftx/__init__.py
+++ jdftx/__init__.py
@@ -1,1 +1,10 @@
-from jdftx import *
+from .jdftx_unitcell import nuc as jdnuc
+from .jdftx_lattice import lattice as jdlat
+from .jdftx_output import output as jdout
+
+from .jdftx_constants import bohr_as_angstrom, hartree_as_eV, \
+                             angstrom_as_bohr, eV_as_hartree
+
+__all__ = ['jdnuc', 'jdlat', 'jdout',
+           'bohr_as_angstrom', 'hartree_as_eV',
+           'angstrom_as_bohr', 'eV_as_hartree']
